c86f19b3d6ac6934c3faf8024337befa86708534
STPlayz <sonniethomas1999@gmail.com>
2022-12-26 00:51:39 +0000

    Installed AJV package and updated some packages

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Amplify } from '@aws-amplify/core';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 // const root = ReactDOM.createRoot(
 //   document.getElementById('root') as HTMLElement
